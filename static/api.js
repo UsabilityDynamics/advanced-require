6b115b689669c4f79c10a85d4c5e2332d91a8b0a
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-07-12 18:40:56 -0400

    Setting up initial structure

---
+++ static/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [],
+    "modules": [
+        "advanced-require"
+    ],
+    "allModules": [
+        {
+            "displayName": "advanced-require",
+            "name": "advanced-require",
+            "description": "AdvancedRequire Module\n\n-"
+        }
+    ]
+} };
+});
